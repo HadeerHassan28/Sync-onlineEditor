2730f6b098f3a0b189a4deebed2741f6cacf7dcf
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-11-03 11:45:10 +0200

    css is done

--- src/component/CodeEditor/CodeEditor.jsx
+++ src/component/CodeEditor/CodeEditor.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
-const CodeEditor = ({ code, onChange }) => {
+const CodeEditor = ({ code, onChange, language }) => {
   return (
-    <MonacoEditor
-      width="100%"
-      height="600"
-      language="html"
-      theme="vs-dark"
-      value={code}
-      options={{
-        selectOnLineNumbers: true,
-      }}
-      onChange={onChange}
-    />
+    <>
+      <MonacoEditor
+        width="100%"
+        height="300"
+        language={language}
+        theme="vs-dark"
+        value={code}
+        options={{
+          selectOnLineNumbers: true,
+        }}
+        onChange={onChange}
+      />
+    </>
   );
 };
 
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Home.module.css";
 import CodeEditor from "../CodeEditor/CodeEditor";
+
 const Home = () => {
-  const [code, setCode] = useState("<h3>Please Enter the HTML code</h3>");
+  const [htmlCode, setHtmlCode] = useState(
+    "<h3>Please Enter the HTML code</h3>"
+  );
+  const [cssCode, setCssCode] = useState(null);
+  useEffect(() => {
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = cssCode;
+    document.head.appendChild(styleElement);
+
+    return () => {
+      document.head.removeChild(styleElement);
+    };
+  }, [cssCode]);
   return (
     <>
-      <div className="App container border my-5">
-        <header className="App-header mt-3 text-center  p-2">
+      <div className="App container border my-5 pb-5">
+        <header className="App-header mt-3 text-center p-2">
           <h1 className="title">
             <u>
               <strong>Online Code Editor</strong>
@@ -16,14 +29,23 @@ const Home = () => {
         <div className="container mt-3">
           <div className="row">
             <div className="col-md-6">
-              <CodeEditor code={code} onChange={setCode} />
+              <h4 className="text-primary">HTML</h4>
+              <CodeEditor
+                code={htmlCode}
+                onChange={setHtmlCode}
+                language="html"
+              />
+              <br />
+              <h4 className="text-primary">CSS</h4>
+              <CodeEditor code={cssCode} onChange={setCssCode} language="css" />
             </div>
-            <div className="col-md-6 output-container output">
+
+            <div className="col-md-6 output-container ">
               <h2 className="outTitle">Output:-</h2>
               <br />
               <div
                 className="output"
-                dangerouslySetInnerHTML={{ __html: code }}
+                dangerouslySetInnerHTML={{ __html: htmlCode }}
               />
             </div>
           </div>
